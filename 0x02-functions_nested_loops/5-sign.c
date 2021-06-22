a2eafb715233fa195f712a81974b1bcdac4a6150
MarkTanui <marktanui75@gmail.com>
2021-06-22 15:35:48 +0300

    Nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,20 +11,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar('\n');
 		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar('\n');
 		return (-1);
 	}
+	_putchar('\n')
 	return (0);
 }

d22a4c366ac9c82521dad3ca756f6865926374ce
MarkTanui <marktanui75@gmail.com>
2021-06-22 11:45:34 +0300

    Nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - prints Holberton
+ * Return: Always 0
+*/
+
+int main(void)
+{	_putchar('H');
+	_putchar('0');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}

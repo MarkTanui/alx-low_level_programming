081c731d437635e41e7a24ad9ea1c7386fb83089
MarkTanui <marktanui75@gmail.com>
2021-06-22 15:58:05 +0300

    Nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,13 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+		_putchar(a);
+
 		for (b = 0; b <= 0; b++)
 		{
-			c = a * b;
-			_putchar(a);
 			_putchar(',');
 			_putchar(' ');
+			c = a * b;
 			_putchar(c);
 		}
 		_putchar('\n');

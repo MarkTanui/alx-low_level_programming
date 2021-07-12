2b31e51f431041a385949dd81a4ba2f19b37a402
MarkTanui <marktanui75@gmail.com>
2021-07-12 21:02:03 +0300

    Argc and Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: counter
+ * @argv: vector
+ * Return: Always (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguments, followed by a new line.
+ * @argc: counter
+ * @argv: vector
+ * Return: Always (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

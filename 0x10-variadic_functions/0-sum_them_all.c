1561427a02c3a759f9b06fd695b5a08019c249c1
MarkTanui <marktanui75@gmail.com>
2021-07-22 22:28:16 +0300

    variadic function tasks

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * sum_them_all - sums all arguments supllied
+ * @n: number of arguments
+ * @...: variadic arguments
+ * Return: sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+
+	va_start(ap, n);
+
+	int sum = 0, i;
+
+	if (n == NULL)
+	return (0);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - print numbers supllied to the funtion
+ * @separator: string to follow number
+ * @n: number of arguments passed
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+
+	va_start(ap, n);
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%d ", va_arg(ap, int));
+		}
+		printf("%d%s ", va_arg(ap, int), separator);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
